f0e896e74b406507dab0bb305fccc7e431b32b63
Anton Lindroth <ntoonio@gmail.com>
2022-06-16 01:33:33 +0200

    Fixed case where it said control number was 10 instead of 0. Added padding to pre

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 function calculate() {
 	reset()
 
-	const nr = document.getElementById("numbersInput").value
+	var nr = document.getElementById("numbersInput").value
 
-	if (nr.length != 9) {
-		alert("Indatan måste vara exakt 9 siffror lång")
+	const matches = nr.match( /(\d{2})?(\d{6}(\+|-)?\d{3})/ )
+
+	if (matches == null || matches.length < 3) {
+		alert("Inte ett giltigt personnummer ange ÅÅMMDD-NNN")
 		document.getElementById("calculation").style.display = "none"
 		return
 	}
 
+	nr = matches[2].replace("-", "").replace("+", "")
+	console.log(matches)
+	const sign = (matches.length >= 4 && matches[3] != undefined) ? matches[3] : "-"
+
 	var nrOut = ""
-	iterate(function(i) {
+	for (var i = 0; i < 9; i++) {
 		nrOut += nr.charAt(i) + "\t"
-	})
+
+		if (i == 5) {
+			nrOut += sign + "\t"
+		}
+	}
 
 	addText(1, nrOut)
-	addText(1, "*	*	*	*	*	*	*	*	*")
-	addText(1, "2	1	2	1	2	1	2	1	2")
-	addText(1, "=	=	=	=	=	=	=	=	=")
+	addText(1, "*	*	*	*	*	*		*	*	*")
+	addText(1, "2	1	2	1	2	1		2	1	2")
+	addText(1, "=	=	=	=	=	=		=	=	=")
 
 	var multOut = []
 	var multOutString = ""
 
-	iterate(function(i) {
+	for (var i = 0; i < 9; i++) {
 		const multiplier = (i + 1) % 2 == 0 ? 1 : 2
 		const result = parseInt(nr.charAt(i)) * multiplier
 		multOut.push(result)
 		multOutString += result.toString() + "\t"
-	})
+
+		if (i == 5) {
+			multOutString += "\t"
+		}
+	}
 
 	addText(1, multOutString)
 	
@@ -47,13 +61,12 @@ function calculate() {
 
 	addText(4, "10 - " + modulusOut + " = " + convertOut)
 
-	addText(5, convertOut)
-}
-
-function iterate(closure) {
-	for (var i = 0; i < 9; i++) {
-		closure(i)
+	// In the edge case where we get 10, show the specific 10-left explanation
+	if (convertOut == 10) {
+		document.getElementById("10left").hidden = false
 	}
+
+	addText(5, convertOut % 10)
 }
 
 function addText(nr, val) {
